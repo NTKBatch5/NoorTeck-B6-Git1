a2a793300ee04e098ed8c625d6123215eb4118a6
ntkb4 <ntkjasur@gmail.com>
2022-05-05 19:18:02 -0400

    more changes

--- src/test/java/demo/DemoTest.java
+++ src/test/java/demo/DemoTest.java
@@ -4,6 +4,12 @@ public class DemoTest {
 	
 	public static void main(String[] args) {
 		System.out.println("Git Demo...");
+		
+		System.out.println("this is my script...");
+		
+		System.out.println("more scriptting....");
+		
+		System.out.println("cahngess.");
 	}
 
 }
