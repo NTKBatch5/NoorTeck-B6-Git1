95fc0583e00d6581ed55ad9e4e1a807086be0042
NTKBatch5 <94854058+NTKBatch5@users.noreply.github.com>
2022-05-08 12:53:09 -0400

    Update DemoThree.java

--- src/test/java/demo/DemoThree.java
+++ src/test/java/demo/DemoThree.java
@@ -10,5 +10,10 @@ public class DemoThree {
     public void addNum3(){
     
    System.out.println("adding"); 
+           System.out.println("adding"); 
+    int a = 4;
+    int b = 5;
+    
+      System.out.println( a + b);
   }
 }
